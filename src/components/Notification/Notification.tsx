e2b68f3cfee67728e702ede0a230ec9474b324af
Tema817 <temashatov@gmail.com>
2026-01-16 07:12:00 +0200

    Update Notification.tsx

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,7 +1,8 @@
 import css from './Notification.module.css';
 
 export default function Notification() {
-  return <p className={css.message}>No feedback yet🍹</p>;
+  return <p className={css.message}>No feedback yet🍑</p>;
 
 }
+
 
