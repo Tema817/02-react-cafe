71966ffce72b4c7294024d9354f90bca1a64f354
Tema817 <temashatov@gmail.com>
2026-01-16 07:04:16 +0200

    Homework#2

--- src/components/CafeInfo/CafeInfo.tsx
+++ src/components/CafeInfo/CafeInfo.tsx
@@ -5,9 +5,10 @@ export default function CafeInfo() {
     <div className={css.container}>
       <h1 className={css.title}>Sip Happens Café</h1>
       <p className={css.description}>
-        Please rate our service by selecting one of the options below.🍑
+        Please rate our service by selecting one of the options below.
       </p>
     </div>
   );
 
 }
+
